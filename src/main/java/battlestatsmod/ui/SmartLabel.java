2a0d43b23b1198c0f5814186b05283f5b3dc0feb
Ben Rollin <ben.rollin@clever.com>
2024-07-16 04:13:09 -0700

    smart labels

---
+++ src/main/java/battlestatsmod/ui/SmartLabel.java
@@ -0,0 +1,27 @@
+package battlestatsmod.ui;
+
+import battlestatsmod.util.RenderingUtils;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+
+public class SmartLabel extends Label {
+    private float lineWidth, lineSpacing;
+
+    public SmartLabel(String text, BitmapFont font, float x, float y, float lineWidth, float lineSpacing, Color color) {
+        super(text, font, x, y, color);
+        this.lineWidth = lineWidth;
+        this.lineSpacing = lineSpacing;
+
+        this.textHeight = font.getLineHeight();
+        this.textWidth = FontHelper.getSmartWidth(font, text, lineWidth, lineSpacing) / Settings.scale;
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        RenderingUtils.renderSmartText(sb, font, text, x * Settings.scale, y * Settings.scale,
+                lineWidth * Settings.scale, lineSpacing * Settings.scale, color);
+    }
+}
